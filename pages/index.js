28d1baa158de47ec92f223e7225f33eb1e5571d4
ittp <61617660+ittp@users.noreply.github.com>
2023-02-05 21:17:23 +0300

    3 files modified, 1 added

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,16 @@
 import axios from 'axios';
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
-
+import {
+  Row,
+  Form,
+  Input,
+  InputNumber,
+  Col,
+  Space,
+  ConfigProvider,
+} from 'antd';
+import DataTable from 'antd-data-table';
 let config = {
   baseURL: process.env.API_URL,
   headers: {
@@ -24,6 +33,7 @@ let {
   findAPortNotInUse,
 } = require('portscanner');
 
+import ping from 'tcp-ping';
 export default function Home({ ctx }) {
   return (
     <div className={styles.container}>
@@ -32,6 +42,16 @@ export default function Home({ ctx }) {
       </Head>
       <main>
         <section className={styles.footer}>Loading...</section>
+
+        <Form name="host">
+          <Form.Item label={'Hostname'}>
+            <Input name="hostname" id="port" />
+          </Form.Item>
+          <Form.Item label={'Port'}>
+            <Input name="port" id="port" />
+          </Form.Item>
+        </Form>
+        {/* <Drawer> */}
 
         <div></div>
       </main>
